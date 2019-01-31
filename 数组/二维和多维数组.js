120fbd8c58c67ff4bb7e6528165c44b31be554a1
fanfanyir <fanfanyir@163.com>
2019-01-31 22:35:50 +0800

    测试代码

--- 数组/二维和多维数组.js
+++ 数组/二维和多维数组.js
@@ -15,6 +15,10 @@ Array.martix = function(numrows, numcols, initial) {
 var nums = Array.martix(5,5,0);
 console.log(nums);
 console.log(nums[1][1]);
+
 var names = Array.martix(3,3,"");
 names[1][2] = "Joe";
 console.log(names[1][2]);
+
+var grades = [[89,77,78],[76,82,81],[91,94,89]];
+console.log(grades[2][2]);
