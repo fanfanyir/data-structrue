bd7a386b9a122e2d2547b759eea1646b672fa9e0
fanfanyir <fanfanyir@163.com>
2019-02-02 22:14:02 +0800

    练习

--- 数组/练习/student.js
+++ 数组/练习/student.js
@@ -1,1 +1,22 @@
 // 创建一个记录学生成绩的对象，提供一个添加成绩的方法，以及一个显示学生平均成绩的方法
+function record () {
+  this.dataStore = [];
+  this.add = add;
+  this.average = average;
+}
+
+function add(temp) {
+  this.dataStore.push(temp)
+}
+
+function average () {
+  var total = 0;
+  for(var i = 0;i< this.dataStore.length;i++){
+    total += this.dataStore[i];
+  }
+  return total/this.dataStore.length;
+}
+
+var records = new record();
+records.add();
+records.average();
--- 数组/练习/weeklyTemps.js
+++ 数组/练习/weeklyTemps.js
@@ -1,2 +1,34 @@
 // 修改前面出现过的 weeklyTemps 对象，使它可以使用一个二维数组来存储每月的有用数据。
 // 增加一些方法用以显示月平均数，具体某一周平均数和所有周的平均数
+
+function weeklyTemps() {
+  this.dataStore = dataStore;
+  this.add = add;
+  this.average = average;
+}
+
+function add(mon, temp) {
+  var mons = this.dataStore[mon];
+  if(mons instanceof Array){
+    mons.push(temp);
+  }else{
+    mons = [];
+  }
+}
+
+function monAverage (mon) {
+  var total = 0;
+  for(var i = 0;i< this.dataStore[mon].length;i++){
+    total += this.dataStore[mon][i];
+  }
+  return total/this.dataStore[mon].length;
+}
+
+function allWeeksAverage () {
+  var total = 0;
+  for(var i = 0;i< this.dataStore.length;i++){
+    total += this.monAverage[i];
+  }
+  return total/this.dataStore.length;
+}
+var records = new record();
